9e7d4807624c24287aaa19928a0177fabe580237
Sandro Covo <sandro@sandrocovo.ch>
2023-03-21 09:38:27 +0100

    add export action to questions

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,9 +1,9 @@
-from chatbot.models import Question
-
 """Admin configuration."""
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportActionMixin
 
-from chatbot.models import Chatbot, Realm, Text
+from chatbot.models import Chatbot, Question, Realm, Text
 
 # Register your models here.
 
@@ -12,29 +12,38 @@ from chatbot.models import Chatbot, Realm, Text
 class RealmAdmin(admin.ModelAdmin):
     """Admin for realm."""
 
-    list_display = ['slug']
+    list_display = ["slug"]
 
 
 @admin.register(Chatbot)
 class ChatbotAdmin(admin.ModelAdmin):
     """Admin for chatbot."""
 
-    list_display = ['name', 'slug', 'realm']
-    list_filter = ['realm']
+    list_display = ["name", "slug", "realm"]
+    list_filter = ["realm"]
 
 
 @admin.register(Text)
 class TextAdmin(admin.ModelAdmin):
     """Admin for texts."""
 
-    list_display = ['url', 'page', 'realm']
-    list_filter = ['realm']
+    list_display = ["url", "page", "realm"]
+    list_filter = ["realm"]
+
+
+class QuestionResource(resources.ModelResource):
+    class Meta:
+        model = Question
 
 
 @admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(ExportActionMixin, admin.ModelAdmin):
     """Admin for questions."""
 
-    list_display = ['question', 'created_at', 'bot', 'user']
-    list_filter = ['bot']
-    readonly_fields = ['context', 'bot']
+    resource_class = QuestionResource
+
+    list_display = ["question", "created_at", "bot", "user", "count"]
+    list_filter = ["bot"]
+    readonly_fields = ["context", "bot"]
+
+    search_fields = ["question"]
