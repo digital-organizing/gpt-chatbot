a0de760c22547c9afb074f2356f92ddcdf4d4a1b
Sandro Covo <sandro@sandrocovo.ch>
2023-04-04 09:10:07 +0200

    restrict access for non super admins

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,34 +1,74 @@
 """Admin configuration."""
+from typing import Any
+
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 from import_export import resources
-from import_export.admin import ExportActionMixin
 
 from chatbot.models import Chatbot, Question, Realm, Text
 
+
 # Register your models here.
+#
+class RestrictedAdminMixin:
+    user_relation = "users"
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        return queryset.filter(**{self.user_relation: request.user})
 
 
 @admin.register(Realm)
-class RealmAdmin(admin.ModelAdmin):
+class RealmAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     """Admin for realm."""
 
     list_display = ["slug"]
 
+    def get_form(self, request: Any, *args, **kwargs: Any) -> Any:
+        if not request.user.is_superuser:
+            self.exclude = [
+                "openai_key",
+                "openai_org",
+            ]
+
+        return super().get_form(request, *args, **kwargs)
+
 
 @admin.register(Chatbot)
-class ChatbotAdmin(admin.ModelAdmin):
+class ChatbotAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     """Admin for chatbot."""
 
     list_display = ["name", "slug", "realm"]
     list_filter = ["realm"]
 
+    def get_form(self, request: Any, *args, **kwargs: Any) -> Any:
+        if not request.user.is_superuser:
+            self.exclude = [
+                "openai_key",
+                "realm",
+                "model",
+                "model_max_tokens",
+                "openai_org",
+                "max_tokens",
+                "users",
+                "restricted",
+                "slug",
+                "skipt_context",
+            ]
+
+        return super().get_form(request, *args, **kwargs)
+
 
 @admin.register(Text)
-class TextAdmin(admin.ModelAdmin):
+class TextAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     """Admin for texts."""
 
     list_display = ["url", "page", "realm"]
     list_filter = ["realm"]
+    user_relation = "realm__users"
 
 
 class QuestionResource(resources.ModelResource):
@@ -37,7 +77,7 @@ class QuestionResource(resources.ModelResource):
 
 
 @admin.register(Question)
-class QuestionAdmin(ExportActionMixin, admin.ModelAdmin):
+class QuestionAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     """Admin for questions."""
 
     resource_class = QuestionResource
@@ -47,3 +87,5 @@ class QuestionAdmin(ExportActionMixin, admin.ModelAdmin):
     readonly_fields = ["context", "bot"]
 
     search_fields = ["question"]
+
+    user_relation = "bot__users"
